29255ec9edda989e4591d00ab01113c69a7bdeca
LanRhyme <xiao_ren233@foxmail.com>
2025-10-04 20:05:17 +0800

    修改渐变效果

--- app/src/main/java/com/lanrhyme/shardlauncher/ui/theme/Color.kt
+++ app/src/main/java/com/lanrhyme/shardlauncher/ui/theme/Color.kt
@@ -5,38 +5,38 @@ import androidx.compose.ui.graphics.Color
 // Purple Theme Colors (Optimized)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Pink80 = Color(0xFFE8C5DD) // Changed from 0xFFEFB8C8
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = Color(0xFF755880) // Changed from 0xFF7D5260
 
 // Green Theme Colors
 val Green40 = Color(0xFF477442)
 val GreenGrey40 = Color(0xFF55624C)
-val GreenTertiary40 = Color(0xFF19686A)
+val GreenTertiary40 = Color(0xFF2E6C5C) // Changed from 0xFF19686A
 
 val Green80 = Color(0xFFB4D39A)
 val GreenGrey80 = Color(0xFFECF6E4)
-val GreenTertiary80 = Color(0xFF7ED0D2)
+val GreenTertiary80 = Color(0xFF90C8B0) // Changed from 0xFF7ED0D2
 
 // Blue Theme Colors
 val Blue40 = Color(0xFF0061A4)
 val BlueGrey40 = Color(0xFF535F70)
-val BlueTertiary40 = Color(0xFF6B5778)
+val BlueTertiary40 = Color(0xFF405D8C) // Changed from 0xFF6B5778
 
 val Blue80 = Color(0xFF9BCAFF)
 val BlueGrey80 = Color(0xFFBFC6D4)
-val BlueTertiary80 = Color(0xFFD6BAF5)
+val BlueTertiary80 = Color(0xFFB8C0E0) // Changed from 0xFFD6BAF5
 
 // Golden Sunset Theme (replaces Cyan)
 val Cyan40 = Color(0xFF7A5900)
 val CyanGrey40 = Color(0xFF6F5D40)
-val CyanTertiary40 = Color(0xFF9A4628)
+val CyanTertiary40 = Color(0xFF8C5C1C) // Changed from 0xFF9A4628
 
 val Cyan80 = Color(0xFFF4C048)
 val CyanGrey80 = Color(0xFFDCC6A1)
-val CyanTertiary80 = Color(0xFFFFB59D)
+val CyanTertiary80 = Color(0xFFFBC87C) // Changed from 0xFFFFB59D
 
 // Common Colors for Dark Theme
 val DarkBackgroundColor = Color(0xFF1B1B1B)
